b93b01e0801bbf74d2e39b856eb9e64c93c6e44e
kessjelly <jessicaskelly01@gmail.com>
2025-11-20 07:34:01 +1000

    Added photo API key and URL

--- vite-project/src/Dictionary.jsx
+++ vite-project/src/Dictionary.jsx
@@ -17,6 +17,9 @@ export default function Dictionary() {
     const apiKey = "3b0e0f1639296410oabf7a45tcd4001b";
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let photoApiKey = "3b0e0f1639296410oabf7a45tcd4001b";
+    let photoApiUrl = `https://api.shecodes.io/images/v1/search?query={query}&key=${key}`;
   }
 
   function handleKeywordChange(event) {
