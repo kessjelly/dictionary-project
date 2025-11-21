5b189e101671e2f49f57bcae5bcfd3e54d0c917b
kessjelly <jessicaskelly01@gmail.com>
2025-11-21 10:45:14 +1000

    Updated link on photos

--- vite-project/src/Photos.jsx
+++ vite-project/src/Photos.jsx
@@ -8,11 +8,13 @@ export default function Photos({ photos }) {
     <div className="Photos row mt-5 gy-3">
       {photos.map((photo, index) => (
         <div className="col-4" key={index}>
-          <img
-            src={photo.src.landscape}
-            alt={photo.alt}
-            className="img-fluid"
-          />
+          <a href={photo.src.original} target="_blank" rel="noreferrer">
+            <img
+              src={photo.src.landscape}
+              alt={photo.alt}
+              className="img-fluid"
+            />
+          </a>
         </div>
       ))}
     </div>
